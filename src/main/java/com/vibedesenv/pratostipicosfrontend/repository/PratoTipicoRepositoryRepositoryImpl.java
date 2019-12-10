3eae15449d7cddee06659795a3607e08b7b9f1bd
Carlos Caldas <carlos.caldas@vibedesenv.com>
2019-12-10 14:58:15 -0300

    formatando decimal

--- src/main/java/com/vibedesenv/pratostipicosfrontend/repository/PratoTipicoRepositoryRepositoryImpl.java
+++ src/main/java/com/vibedesenv/pratostipicosfrontend/repository/PratoTipicoRepositoryRepositoryImpl.java
@@ -19,14 +19,14 @@ public class PratoTipicoRepositoryRepositoryImpl implements PratoTipicoRepositor
 		prato1.setId(1);
 		prato1.setNome("nome");
 		prato1.setDescricao("descricao do prato");
-		prato1.setValor(new BigDecimal(20.0));
+		prato1.setValor(new BigDecimal(20.55));
 		list.add(prato1);
 		
 		PratoTipico prato2 = new PratoTipico();
 		prato2.setId(2);
 		prato2.setNome("nome");
 		prato2.setDescricao("descricao do prato com mais detalhes");
-		prato2.setValor(new BigDecimal(10.0));
+		prato2.setValor(new BigDecimal(.5));
 		list.add(prato2);
 		
 		return list;
